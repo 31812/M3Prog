3a7e6615b00446e4e20a5b83cc35f46623080a4f
31812 <31812@ma-web.nl>
2024-03-07 10:54:18 +0100

    variabelen

---
+++ m3progproject/public/01/variabelen.php
@@ -0,0 +1,25 @@
+<?php
+$naam = "Elias";
+$woonplaats = "Amstel";
+$leeftijd = 16;
+$temperatuur = 6;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<header>
+</header>
+<body>
+    <main>
+    <p>
+        Mijn naam is <?php echo $naam; ?> ik woon op de <?php echo $woonplaats; ?>, ik ben <?php echo $leeftijd; ?> jaar oud en het is <?php echo $temperatuur; ?> graden buiten
+    </p>
+</main>
+</body>
+<footer>
+</footer>
+</html>
