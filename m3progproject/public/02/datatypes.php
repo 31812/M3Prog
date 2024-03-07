df1381591445e58c4dcd452f60fec6d273cccdc8
31812 <31812@ma-web.nl>
2024-03-07 14:27:51 +0100

    datatypes

---
+++ m3progproject/public/02/datatypes.php
@@ -0,0 +1,23 @@
+<?php
+
+$huisnummer = 45;
+
+$rijbewijsgeslaagd = false;
+
+$literbenzine = 3.10;
+
+$Waarde = null;
+
+?>
+
+<?php
+
+echo "mijn huisnummer is ", $huisnummer;
+echo "<br>";
+echo "ben ik geslaagd voor mijn rijbewijs examen? ", $rijbewijsgeslaagd;
+echo "<br>";
+echo "een liter benzine kost ", $literbenzine, " euro";
+echo "<br>";
+echo $Waarde;
+
+?>
