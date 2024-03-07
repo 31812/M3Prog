98053a1488e70e338bdb1c5d9775eda804ae712c
31812 <31812@ma-web.nl>
2024-03-07 14:54:09 +0100

    variabelen02

---
+++ m3progproject/public/02/variabelen.php
@@ -0,0 +1,25 @@
+<?php
+
+    $auto2 = "goated";
+    $_naam = "Elijah";
+    //$auto's = "Lambo-Bugatti"; werkt niet vanwege apostrof//
+    //$auto/onderdelen = "Motor"; werkt niet vanwege "/"
+    //$huis&tuin = "Rijtjes"; werkt niet vanwege "&"
+    $naw = "hell nah";
+    //$22 = 21; nummers kunnen geen variabel zijn//
+    //$$auto = "Huracan"; werkt niet vanwege twee "$"//
+    $kleurVanAuto = "zwart";
+    //$software-dev = "ikke"; werkt niet vanwege "-"//
+?>
+
+<?php
+
+echo $auto2;
+echo "<br>";
+echo $_naam;
+echo "<br>";
+echo $naw;
+echo "<br>";
+echo $kleurVanAuto;
+
+
